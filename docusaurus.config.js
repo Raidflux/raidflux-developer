e8fcfd30ba772a94adcd4072ef53fd0a5e2335e3
Corne <cornesteenhuis@hotmail.com>
2021-08-04 11:19:42 +0200

    add basic docs setup

---
+++ docusaurus.config.js
@@ -0,0 +1,131 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'Raidflux',
+  tagline: 'Raidflux documentation',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'raidflux', // Usually your GitHub org/user name.
+  projectName: 'raidflux', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Raidflux',
+      logo: {
+        alt: 'Raidflux Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'doc',
+          docId: 'intro',
+          position: 'left',
+          label: 'Documentation',
+        },
+        {
+          href: 'https://console.raidflux.com',
+          label: 'Login',
+          position: 'right',
+        },
+        {
+          href: 'https://raidflux.com',
+          label: 'Landing page',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/Raidflux',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'LinkedIn',
+              href: 'https://www.linkedin.com/company/raidflux/',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discord.com/invite/JwJ3wpbHU2',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/raidflux',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/Raidflux',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Raidflux`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: false,
+      respectPrefersColorScheme: true,
+      switchConfig: {
+        darkIcon: '🌙',
+        lightIcon: '🌞',
+        darkIconStyle: {
+          marginLeft: '2px',
+        },
+        lightIconStyle: {
+          marginLeft: '1px',
+        },
+      },
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+  plugins: [
+    require.resolve('docusaurus-lunr-search')
+  ],
+};
